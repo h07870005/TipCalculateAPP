cbcb1f72a51add3f7911bd25263ca73ba6bb44b3
h07870005 <h07870005@yahoo.com.tw>
2017-08-19 20:43:15 +0800

    Initial Commit

---
+++ 小費計算APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  小費計算APP
+//
+//  Created by Huang Hao on 2017/8/19.
+//  Copyright © 2017年 Huang Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
